e9b77c916ecb0c475d7ae9729869e5fd30907f6f
Jae-Won Chung <jwnchung@umich.edu>
2023-06-22 22:21:32 -0400

    Don't clone in

--- src/sync.rs
+++ src/sync.rs
@@ -1,14 +1,14 @@
 use std::fs::{File, OpenOptions};
 
-pub struct LockedFile {
+pub struct LockedFile<'a> {
     /// Path to the lock file.
     lock_path: String,
     /// The file this lock protects.
-    file_path: String,
+    file_path: &'a str,
 }
 
-impl LockedFile {
-    pub async fn acquire(file_path: &str) -> Self {
+impl<'a> LockedFile<'a> {
+    pub async fn acquire(file_path: &'a str) -> LockedFile<'a> {
         let lock_path = format!("{}.lock", &file_path);
         while OpenOptions::new()
             .write(true)
@@ -20,21 +20,21 @@ impl LockedFile {
         }
         Self {
             lock_path,
-            file_path: file_path.to_owned(),
+            file_path,
         }
     }
 
     pub fn read_handle(&self) -> File {
-        File::open(&self.file_path).unwrap_or_else(|_| panic!("Failed to open {}", &self.file_path))
+        File::open(self.file_path).unwrap_or_else(|_| panic!("Failed to open {}", &self.file_path))
     }
 
     pub fn write_handle(&self) -> File {
-        File::create(&self.file_path)
+        File::create(self.file_path)
             .unwrap_or_else(|_| panic!("Failed to create {}", &self.file_path))
     }
 }
 
-impl Drop for LockedFile {
+impl Drop for LockedFile<'_> {
     fn drop(&mut self) {
         std::fs::remove_file(&self.lock_path)
             .unwrap_or_else(|_| panic!("Failed to remove lock file {}", &self.lock_path));
